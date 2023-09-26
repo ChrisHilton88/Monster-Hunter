29f3ee31a389650d21b1b56949702265d4e9dd38
ChrisHilton88 <christopherhilton88@gmail.com>
2023-09-26 14:38:27 +0300

    Built a Critical Hit prefab and adjusted coding logic to allow for Critical Hits

--- Assets/Scripts/Player/WeaponShooting.cs
+++ Assets/Scripts/Player/WeaponShooting.cs
@@ -1,12 +1,12 @@
-using System;
 using System.Collections;
 using UnityEngine;
 
 public class WeaponShooting : MonoBehaviour
 {
+    private int _minDiceRoll = 0, _maxDiceRoll = 101; 
+
 
     private readonly Vector3 _reticulePos = new Vector3(0.5f, 0.5f, 0);
-    private readonly float _damagePopUpOffset = 0.25f;
 
     private AudioSource _audioSource;
     [SerializeField] private AudioClip _weaponFiredClip;
@@ -25,15 +25,6 @@ public class WeaponShooting : MonoBehaviour
         private set { _canShoot = value; }  
     }
 
-    private int _damageDealt;
-    public int DamageDealt
-    {
-        get { return _damageDealt; }
-        set { _damageDealt = value; }
-    }
-
-    // Event - When a Game Object takes damage
-    public static Action<int> OnDamageDealt;
 
 
     void Start()
@@ -44,13 +35,6 @@ public class WeaponShooting : MonoBehaviour
         _audioSource = GetComponent<AudioSource>();
     }
 
-
-    // Player shoots -> Player checks if object is an enemy -> Checks if enemy health > 0 -> Trigger event to take damage
-    // As this is the script that holds the event, we need to check that we have subscribers before calling the event. Subscribers responsibility to subscribe/desubscribe
-
-
-    // Called from InputManager
-    // Request a bullet from ObjectPoolManager and Shoot.
     public void ShootBullet()
     {
         if (UIManager.Instance.AmmoCount > 0)               // Check that there is a bullet available
@@ -64,12 +48,11 @@ public class WeaponShooting : MonoBehaviour
                 ObjectPoolManager.Instance.RequestBullet(hitInfo);      
                 UIManager.Instance.UpdateAmmoCount(1);
 
-                // Check if the Game Object hit has the interface IDamageable on it
-                if(hitInfo.transform.GetComponent<IDamageable>() != null)
+                if(hitInfo.transform.GetComponent<IDamageable>() != null)       // Check if GameObject has IDamageable
                 {
-                    IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();     // Store object reference to the script
-                    Vector3 _damageSpawnPos = hitInfo.transform.GetChild(0).transform.position;     // All enemies first child is the spawn box 
-
+                    IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();    
+                    Vector3 _damageSpawnPos = hitInfo.transform.GetChild(0).transform.position;     // All enemies first child is the spawn box
+                    
                     int damageDealt = RandomDamageDealt();
                     FloatingCombatTextPopUp.Instance.InstantiateDamagePopUp(_damageSpawnPos, damageDealt);
                     damageable.ReceiveDamage(damageDealt);
@@ -91,11 +74,10 @@ public class WeaponShooting : MonoBehaviour
     //        return;
     //}
 
-    // Returns a random int value
-    int RandomDamageDealt()
+    
+    int RandomDamageDealt()     // Roll a dice and return an int value
     {
-        int randomNumber = UnityEngine.Random.Range(20, 41);
-        Debug.Log("random value: " + randomNumber); 
+        int randomNumber = Random.Range(_minDiceRoll, _maxDiceRoll);
         return randomNumber;
     }
 
--- Assets/Scripts/UI/FloatingCombatTextPopUp.cs
+++ Assets/Scripts/UI/FloatingCombatTextPopUp.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 public class FloatingCombatTextPopUp : MonoSingleton<FloatingCombatTextPopUp>
 {
     [SerializeField] GameObject _damageTextPrefab;
+    [SerializeField] GameObject _criticalTextPrefab;
 
-    private float _minValue = -0.3f, _maxValue = 0.3f;
+    private float _minRange = -0.3f, _maxRange = 0.3f;      // min and max range for vector position
 
 
     // Create Damage PopUp Text
     public void InstantiateDamagePopUp(Vector3 pos, int damage)
     {
-        GameObject newPopUp = Instantiate(_damageTextPrefab, pos + RandomVector(), Quaternion.identity);     // Instantiate a new _damageTextPrfab GameObject and get a reference to it
-        TextMeshProUGUI temp = newPopUp.transform.GetComponentInChildren<TextMeshProUGUI>();      // Create a temporary TMPUGUI class to hold a reference to the component
-        temp.text = damage.ToString();       // Assign the .text field from the component to the string value passed in through the parameter
+        GameObject newPopUp;
+
+        if(damage > 70)
+        {
+            newPopUp = Instantiate(_criticalTextPrefab, pos + RandomVectorPos(), Quaternion.identity);
+        }
+        else
+        {
+            newPopUp = Instantiate(_damageTextPrefab, pos + RandomVectorPos(), Quaternion.identity);
+        }
+
+        TextMeshProUGUI temp = newPopUp.transform.GetComponentInChildren<TextMeshProUGUI>();
+        temp.text = damage.ToString();
     }
 
+
     // Generate a random Vector3
-    Vector3 RandomVector()
+    Vector3 RandomVectorPos()
     {
-        Vector3 newVector = new Vector3(Random.Range(_minValue, _maxValue), Random.Range(_minValue, _maxValue), 0);
+        Vector3 newVector = new Vector3(Random.Range(_minRange, _maxRange), Random.Range(_minRange, _maxRange), 0);
         return newVector;
     }
+
 }
 
 

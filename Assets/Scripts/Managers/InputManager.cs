a3f0516c0b44ef8837dc1b0ca5ad45bec49603c2
ChrisHilton88 <christopherhilton88@gmail.com>
2023-11-30 13:48:36 +0200

    Fixed issue with player not moving and removed a large amount of overhead for features/abilities that came with FPSController that weren't required

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,92 +1,76 @@
 using GameDevHQ.FileBase.Plugins.FPS_Character_Controller;
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
-    [SerializeField] private GameObject _managers;
 
     PlayerInputActions _playerInputActions;
     FPS_Controller _fpsController;
     PlayerCameraController _playerCameraController;
     WeaponShooting _weaponShooting; 
     GameManager _gameManager;
-    Ammo _ammo;
 
 
 
-
-    // Subscribe to events
-    void OnEnable()
+    #region Initialisation
+    private void OnEnable()
     {
         _playerInputActions = new PlayerInputActions();
         _playerInputActions.Player.Enable();
         _playerInputActions.Player.LookMovement.performed += LookMovementPerformed;
         _playerInputActions.Player.PlayerMovement.performed += PlayerMovementPerformed;
         _playerInputActions.Player.PlayerMovement.canceled += PlayerMovementCanceled;
-        _playerInputActions.Player.Jump.performed += JumpPerformed;
         _playerInputActions.Player.Shoot.performed += ShootPerformed;
         _playerInputActions.Player.Shoot.canceled += ShootCanceled;
         _playerInputActions.Player.Reload.performed += ReloadPerformed;
         _playerInputActions.Player.Reload.canceled += ReloadCanceled;
-        _playerInputActions.Player.Crouch.performed += CrouchPerformed;
-        _playerInputActions.Player.Crouch.canceled += CrouchCanceled;
-        _playerInputActions.Player.Running.performed += RunningPerformed;
-        _playerInputActions.Player.Running.canceled += RunningCanceled;
         _playerInputActions.Player.SniperZoom.performed += SniperZoomPerformed;
         _playerInputActions.Player.SniperZoom.canceled += SniperZoomCanceled;
         _playerInputActions.Player.Cursor.performed += CursorPerformed;
         //_playerInputActions.Player.OptionsMenu.performed += OptionsMenuPerformed;
     }
 
-    //void OptionsMenuPerformed(InputAction.CallbackContext context)
-    //{
-    //    if (UIManager.Instance.IsOptionsMenuOpen)
-    //    {
-    //        // Close menu
-    //    }
-    //    else
-    //    {
-    //        // Open menu
-    //    }
-    //    Debug.Log("Opened the Options Menu");
-    //}
-
-    void Start()
+    private void OnDisable()
     {
-        _playerCameraController = _player.GetComponent<PlayerCameraController>();   
-        _weaponShooting = _player.GetComponentInChildren<WeaponShooting>(); 
-        _gameManager = _managers.GetComponentInChildren<GameManager>(); 
-        _fpsController = _player.GetComponentInChildren<FPS_Controller>();  
+        _playerInputActions.Player.Disable();
+        _playerInputActions.Player.LookMovement.performed -= LookMovementPerformed;
+        _playerInputActions.Player.PlayerMovement.performed -= PlayerMovementPerformed;
+        _playerInputActions.Player.PlayerMovement.canceled -= PlayerMovementCanceled;
+        _playerInputActions.Player.Shoot.performed -= ShootPerformed;
+        _playerInputActions.Player.Shoot.canceled -= ShootCanceled;
+        _playerInputActions.Player.SniperZoom.performed -= SniperZoomPerformed;
+        _playerInputActions.Player.SniperZoom.canceled -= SniperZoomCanceled;
+        _playerInputActions.Player.Cursor.performed -= CursorPerformed;
+        //_playerInputActions.Player.OptionsMenu.performed -= OptionsMenuCanceled;
     }
 
+    private void Start()
+    {
+        _playerCameraController = _player.GetComponent<PlayerCameraController>();
+        _weaponShooting = _player.GetComponentInChildren<WeaponShooting>();
+        _fpsController = _player.GetComponentInChildren<FPS_Controller>();
+    }
+    #endregion#
 
     #region INPUT ACTION EVENTS
-    
-    void LookMovementPerformed(InputAction.CallbackContext context)
+    private void LookMovementPerformed(InputAction.CallbackContext context)
     {
-        Vector2 lookMovement = context.ReadValue<Vector2>();                    // Cache context callback values
-        _playerCameraController.CameraController(lookMovement);                 // Pass in those values to CameraController() method.
+        _playerCameraController.CameraController(context.ReadValue<Vector2>());                 
     }
 
-    void PlayerMovementPerformed(InputAction.CallbackContext context)
+    private void PlayerMovementPerformed(InputAction.CallbackContext context)
     {
         _fpsController.PlayerMovement(context.ReadValue<Vector2>());    
     }
 
-    void PlayerMovementCanceled(InputAction.CallbackContext context)
+    private void PlayerMovementCanceled(InputAction.CallbackContext context)
     {
         _fpsController.PlayerMovement(Vector2.zero);
     }
 
-    void JumpPerformed(InputAction.CallbackContext context)
-    {
-        _fpsController.Jumping(context.ReadValue<float>());
-    }
-
-    void ShootPerformed(InputAction.CallbackContext context)
+    private void ShootPerformed(InputAction.CallbackContext context)
     {
         if (Ammo.Instance.CanShoot == true)
         {
@@ -123,63 +107,34 @@ public class InputManager : MonoBehaviour
         // TODO: When the player presses the Reload button while the weapon is currently reloading, cancel reload.
     }
 
-    void CrouchPerformed(InputAction.CallbackContext context)
-    {
-        _fpsController.Crouching(context.ReadValue<float>());
-    }
-
-    void CrouchCanceled(InputAction.CallbackContext context)
-    {
-        _fpsController.Crouching(context.ReadValue<float>());
-        Debug.Log("Not Crouching!");
-    }
-
-    void RunningPerformed(InputAction.CallbackContext context)
-    {
-        _fpsController.Running(context.ReadValue<float>());
-    }
-
-    void RunningCanceled(InputAction.CallbackContext context)
-    {
-        _fpsController.Running(context.ReadValue<float>());
-    }
-
-    void SniperZoomPerformed(InputAction.CallbackContext context)
+    private void SniperZoomPerformed(InputAction.CallbackContext context)
     {
         // Start zooming in process
         _fpsController.SniperZoom(context.ReadValue<float>());  
     }
 
-    void SniperZoomCanceled(InputAction.CallbackContext context)
+    private void SniperZoomCanceled(InputAction.CallbackContext context)
     {
         // Stop zooming out process and go back to normal
         _fpsController.SniperZoom(context.ReadValue<float>());
     }
 
-    void CursorPerformed(InputAction.CallbackContext context)
+    private void CursorPerformed(InputAction.CallbackContext context)
     {
         _gameManager.SetCursorStateToNone();
     }
 
-
+    //void OptionsMenuPerformed(InputAction.CallbackContext context)
+    //{
+    //    if (UIManager.Instance.IsOptionsMenuOpen)
+    //    {
+    //        // Close menu
+    //    }
+    //    else
+    //    {
+    //        // Open menu
+    //    }
+    //    Debug.Log("Opened the Options Menu");
+    //}
     #endregion
-
-    // Unsubscribe to events.
-    void OnDisable()
-    {
-        _playerInputActions.Player.Disable();
-        _playerInputActions.Player.LookMovement.performed -= LookMovementPerformed;
-        _playerInputActions.Player.PlayerMovement.performed -= PlayerMovementPerformed;
-        _playerInputActions.Player.PlayerMovement.canceled -= PlayerMovementCanceled;
-        _playerInputActions.Player.Jump.performed -= JumpPerformed;
-        _playerInputActions.Player.Shoot.performed -= ShootPerformed;
-        _playerInputActions.Player.Shoot.canceled -= ShootCanceled;
-        _playerInputActions.Player.Crouch.performed -= CrouchPerformed;
-        _playerInputActions.Player.Crouch.canceled -= CrouchCanceled;
-        _playerInputActions.Player.Running.performed -= RunningPerformed;
-        _playerInputActions.Player.Running.canceled -= RunningCanceled;
-        _playerInputActions.Player.SniperZoom.performed -= SniperZoomPerformed;
-        _playerInputActions.Player.SniperZoom.canceled -= SniperZoomCanceled;
-        _playerInputActions.Player.Cursor.performed -= CursorPerformed;
-    }
 }
--- Assets/Scripts/Player/FPS_Controller.cs
+++ Assets/Scripts/Player/FPS_Controller.cs
@@ -5,33 +5,6 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
     [RequireComponent(typeof(CharacterController))]
     public class FPS_Controller : MonoBehaviour
     {
-        [Header("Controller Info")]
-        [SerializeField]
-        [Tooltip("How fast can the player walk?")]
-        private float _walkSpeed = 3.0f;            // How fast the character is walking.
-        [SerializeField]
-        [Tooltip("How fast can the player run?")]
-        private float _runSpeed = 7.0f;             // How fast the character is running.
-        [SerializeField]
-        [Tooltip("Set the player's gravity multiplier")]
-        private float _gravity = 1.0f;              // How much gravity to apply.
-        [SerializeField]
-        [Tooltip("How high can the player jump?")]
-        private float _jumpHeight = 15.0f;          // How high can the character jump
-        [SerializeField]
-        [Tooltip("Returns true if the player is running")]
-        private bool _isRunning = false;            // Bool to display if we are running.
-        [SerializeField]
-        [Tooltip("Returns true if the player is jumping")]
-        private bool _isJumping = false;            // Bool to display if we are jumping.
-        [SerializeField]
-        [Tooltip("Returns true if the player is crouching")]
-        private bool _crouching = false;            // Bool to display if we are crouched or not.
-
-        private CharacterController _controller;    // Reference variable to the 'CharacterController' component.
-        private float _yVelocity = 0.0f;            // Cache player's 'Y' velocity.
-
-
         [Header("Headbob Settings")]
         [SerializeField]
         [Tooltip("Smooth out the transition from moving to not moving")]
@@ -48,61 +21,50 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
         private float _heightOffset = 0.05f;        // How dramatic the bobbing is.
         private float _timer = Mathf.PI / 2;        // This is where Sin = 1 -- used to simulate walking forward. 
 
+
+        private int minFOV = 30, maxFOV = 55;       // Zoom In/Out min/max 
+
+        private float _walkSpeed = 3.0f;            
+        private float _zoomMultiplier = 0.5f;
+
+        private bool _isZooming = false;
+
+        private Vector2 _movement;
+
+        private CharacterController _controller;    // Reference variable to the 'CharacterController' component.
+
         [SerializeField] private Camera _fpsCamera;
         [SerializeField] private PlayerCameraController _playerCam;
 
-        private int minFOV = 30, maxFOV = 55;
-        private bool _isZooming = false;
-        private float _zoomMultiplier = 0.5f;
-
-        private Vector2 _movement;
-        private Vector3 _crouchingPos = new Vector3(0, -0.3f, 0);
-        private Vector3 _standingPos = new Vector3(0, 0.3f, 0);
 
 
-
-        void Start()
+        #region Initialisation
+        private void Start()
         {
             _controller = GetComponent<CharacterController>();
             _playerCam = GetComponent<PlayerCameraController>();
         }
+        #endregion
 
-        void Update()
+        #region Methods
+        private void Update()
         {
             FPSController(_movement);
-            HeadBobbing(); 
+            HeadBobbing();
         }
 
-        void FPSController(Vector2 movement)
+        private void FPSController(Vector2 movement)
         {
             // Old Input System
-            //float h = Input.GetAxis("Horizontal"); //horizontal inputs (a, d, leftarrow, rightarrow)
-            //float v = Input.GetAxis("Vertical"); //veritical inputs (w, s, uparrow, downarrow)
+            //float h = Input.GetAxis("Horizontal"); //horizontal inputs (a, d, left arrow, right arrow)
+            //float v = Input.GetAxis("Vertical"); //veritical inputs (w, s, up arrow, down arrow)
 
-            Vector3 direction = new Vector3(movement.x, 0, movement.y);                                       // direction to move
-            Vector3 velocity;
+            Vector3 direction = new Vector3(movement.x, 0, movement.y);
+            Vector3 velocity = direction * _walkSpeed;
 
-            // Running
-            if (!_isRunning)                                                                                  // If isRunning is false - Set to walk speed
-                velocity = direction * _walkSpeed;                                                            // velocity is the direction and speed we travel
-            else
-                velocity = direction * _runSpeed;                                                             // Else, set it to running speed.                                            
-
-            // Jumping
-            if (_isJumping)                                                                                   // If player is jumping 
-            {
-                _yVelocity = _jumpHeight;                                                                     //assign the cache velocity to our jump height
-                _isJumping = false;
-            }
-            else
-            {
-                return;
-            }
-
-            velocity.y = _yVelocity;                                                                          // Assign the cached value of our 'Y' velocity.
             velocity = transform.TransformDirection(velocity);
-            
-            _controller.Move(velocity * Time.deltaTime);                                                      // Move the controller 'X' meters per second.
+
+            _controller.Move(velocity * Time.deltaTime);
         }
 
         public void PlayerMovement(Vector2 context)
@@ -110,77 +72,11 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
             _movement = context;
         }
 
-        public void Crouching(float context)
-        {
-            if (_isRunning == false && context == 1)
-            {
-                _crouching = true;
-                _controller.height = 1f;
-                _controller.Move(_crouchingPos);
-                Debug.Log("Cont height: " + _controller.height);
-            }
-            else
-            {
-                _crouching = false;
-                _controller.height = 2f;
-                _controller.Move(_standingPos);
-                Debug.Log("Cont height: " + _controller.height);
-            }
-
-            // Old Input System
-            //if (Input.GetKeyDown(KeyCode.C) && _isRunning == false)
-            //{
-            //    if (_crouching == true)
-            //    {
-            //        _crouching = false;
-            //        _controller.height = 2.0f;
-            //    }
-            //    else
-            //    {
-            //        _crouching = true;
-            //        _controller.height = 1.0f;
-            //    }
-            //}
-        }
-
-        public void Running(float context)
-        {
-            if (_crouching == false && context == 1)
-            {
-                _isRunning = true;
-            }
-            else
-            {
-                _isRunning = false;
-            }
-        }
-
-        public void Jumping(float context)
-        {
-            if (_controller.isGrounded && context == 1)                                                                       // Check if we're grounded.
-            {
-                _isJumping = true;
-                Debug.Log("Controller grounded - Jumping");
-            }
-
-            // Old Input System
-            //if (_controller.isGrounded == true) 
-            //{
-            //    if (Input.GetKeyDown(KeyCode.Space)) 
-            //    {
-            //        _yVelocity = _jumpHeight; 
-            //    }
-            //}
-            //else 
-            //{
-            //    _yVelocity -= _gravity; //subtract gravity from our yVelocity 
-            //}
-        }
-
+        // TODO: LERP Zoom In/Out and lock selection until finished reaching POV size
         public void SniperZoom(float context)
-        { 
+        {
             // Zoom in
-            if(context == 1 && _isZooming == false)
+            if (context == 1 && _isZooming == false)
             {
                 while (_fpsCamera.fieldOfView > minFOV)
                 {
@@ -189,7 +85,7 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
                     if (_fpsCamera.fieldOfView <= minFOV)
                     {
                         _fpsCamera.fieldOfView = minFOV;
-                        
+
                     }
 
                     _isZooming = true;
@@ -198,7 +94,7 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
             // Zoom out
             else
             {
-                while(_fpsCamera.fieldOfView < maxFOV)
+                while (_fpsCamera.fieldOfView < maxFOV)
                 {
                     _fpsCamera.fieldOfView += _zoomMultiplier;
 
@@ -212,7 +108,7 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
             }
         }
 
-        void HeadBobbing()
+        private void HeadBobbing()
         {
             // Old Input System
             //float h = Input.GetAxis("Horizontal");          // Horizontal input.
@@ -220,14 +116,7 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
 
             if (_movement.x != 0 || _movement.y != 0)                                              // Are we moving?
             {
-                if (_isRunning)                                                                    // Check if running.
-                {
-                    _timer += _runFrequency * Time.deltaTime;                                      // Increment timer for our sin/cos waves when running.
-                }
-                else
-                {
-                    _timer += _walkFrequency * Time.deltaTime;                                     // Increment timer for our sin/cos waves when walking.
-                }
+                _timer += _walkFrequency * Time.deltaTime;                                     // Increment timer for our sin/cos waves when walking.
 
                 Vector3 headPosition = new Vector3                                                 // Calculate the head position in our walk cycle.
                     (
@@ -301,8 +190,7 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
             //        _fpsCamera.transform.localPosition = resetHead;         // Assign the head position back to the initial camera position.
             //    }
         }
+        #endregion
     }
-
-
 }
 

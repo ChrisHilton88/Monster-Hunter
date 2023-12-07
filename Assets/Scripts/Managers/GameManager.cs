08c7a3d510c2fb776d545a32470b712aac2244c2
ChrisHilton88 <christopherhilton88@gmail.com>
2023-12-07 15:18:36 +0200

    Built Win condition

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,12 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoSingleton<GameManager>   
 {
-    public bool GameOver { get; private set; }
+    [SerializeField] private int _totalEnemyCount;
+
+    private bool _gameOver;
+
+    #region Properties
+    public int TotalEnemyCount { get { return _totalEnemyCount; } private set { _totalEnemyCount = value; } }
+    public bool GameOver { get { return _gameOver; } private set { _gameOver = value; } }
+
+    #endregion
 
 
+
+    private void OnEnable()
+    {
+        EnemyBase.OnEnemyDeath += WinCount;
+    }
+
+    private void OnDisable()
+    {
+        EnemyBase.OnEnemyDeath -= WinCount; 
+    }
 
     void Start()
     {
+        CalculateTotalEnemyCount();
+        Debug.Log("Total Enemy Count: " + TotalEnemyCount);
         GameOver = false;
         Cursor.lockState = CursorLockMode.Confined;
     }
@@ -19,7 +39,36 @@ public class GameManager : MonoSingleton<GameManager>
         Cursor.lockState = CursorLockMode.None;
     }
 
-    // Sets GameOver state.
+    // Responsible for setting the total enemy count for Win Condition
+    private int CalculateTotalEnemyCount()
+    {
+        // Loop over the count of waves and the count of enemies within the waves
+        for (int i = 0; i < SpawnManager.Instance.WaveList.Count; i++)
+        {
+            for (int j = 0; j < SpawnManager.Instance.WaveList[i].enemyList.Count; j++)
+            {
+                TotalEnemyCount++;
+            }
+        }
+
+        return TotalEnemyCount;
+    }
+
+    // Win Condition decrementer (Win when 0 enemies remain)
+    private void WinCount()
+    {
+        if(TotalEnemyCount <= 0)
+        {
+            GameOver = true;
+            // Win Screen
+        }
+        else
+        {
+            TotalEnemyCount--;
+        }
+    }
+
+    // Sets GameOver state
     public void SetGameOver()
     {
         GameOver = true;

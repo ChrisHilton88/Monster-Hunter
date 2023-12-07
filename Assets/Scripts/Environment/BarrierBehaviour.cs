63ba11f9da9c6644caa25393eb691f8628e4a2a7
ChrisHilton88 <christopherhilton88@gmail.com>
2023-12-07 20:29:05 +0200

    Imported VFX assets and built feature for VFX to play when barrier is destroyed

--- Assets/Scripts/Environment/BarrierBehaviour.cs
+++ Assets/Scripts/Environment/BarrierBehaviour.cs
@@ -1,13 +1,55 @@
+using System.Collections;
 using UnityEngine;
 
 // Responsible for the environmental barrier game object behaviours
 public class BarrierBehaviour : MonoBehaviour, IDamageable
 {
-    private int _health = 500;
+    [SerializeField] private int _health = 500;
+    private int _destroyed = 0;
 
+    private BoxCollider _boxCollider;
+    private MeshRenderer _meshRenderer;
+
+    [SerializeField] private ParticleSystem _particleSystem;
+
+
+    private void Start()
+    {
+        _boxCollider = GetComponent<BoxCollider>(); 
+        _meshRenderer = GetComponent<MeshRenderer>();   
+    }
 
     public void ReceiveDamage(int damageReceived)
     {
         _health -= damageReceived;
+
+        if(_health < _destroyed)
+        {
+            _particleSystem.gameObject.SetActive(true); 
+            _boxCollider.enabled = false;
+            _meshRenderer.enabled = false;
+            StartCoroutine(WaitForParticleSystem());
+        }
+    }
+
+    private void CooldownTimer()
+    {
+        // Keep setting timer back to 0 everytime the object gets hit
+        // When the object hasn't been hit for 5 secs
+        // Start repairing the object
+        // Play particle effect/animation to show it is recharging
+        // Stop playing effects when Health is back to full
+
+        // Restart timer all over again if the object gets shot while repairing itself
+    }
+
+    private IEnumerator WaitForParticleSystem()
+    {
+        while (_particleSystem.isPlaying)
+        {
+            yield return null;
+        }
+
+        _particleSystem.gameObject.SetActive(false);
     }
 }

365fae3347cbff00e166ee7335a229f180ceb800
ChrisHilton88 <christopherhilton88@gmail.com>
2023-12-08 14:29:50 +0200

    Built a DamageableBase class for all damageable objects to inherit from

--- Assets/Scripts/Environment/BarrierBehaviour.cs
+++ Assets/Scripts/Environment/BarrierBehaviour.cs
@@ -1,137 +1,27 @@
-using System.Collections;
-using UnityEngine;
-
 // Responsible for the environmental barrier game object behaviours
-public class BarrierBehaviour : MonoBehaviour, IDamageable
+public class BarrierBehaviour : DamageableBase
 {
-    private int _maxHealth = 500;
-    private int _destroyed = 0;
+    // Sealed modifier - Allows for no further changes to that method if inheriting this class
+    // Barrier specific initialisation values
+    private int _barrierMaxHealth = 500;            
 
-    private float _idleRepairTimer = 5f;         // When the automatic repair of a destroyable object should start
-    private float _repairMultiplier = 10f;
-    [SerializeField] private float _health = 500;
-
-    private BoxCollider _boxCollider;
-    private Coroutine _cooldownTimerRoutine;
-    private Coroutine _destroyRoutine;
-    private Coroutine _repairRoutine;
-    private MeshRenderer _meshRenderer;
-    
-
-    [SerializeField] private ParticleSystem _repairParticleSystem;
-    [SerializeField] private ParticleSystem _destroyedParticleSystem;
-    [SerializeField] private GameObject _repairIcon;
-
-    #region Properties
-    public int MaxHealth { get { return _maxHealth; } }
-    public float RepairMultiplier { get { return _repairMultiplier; } }
-    public float Health { get { return _health; } set { _health = value; } }
-    #endregion
+    private float _barrierIdleRepairTimer = 5f;            
+    private float _barrierRepairMultiplier = 10f;
 
 
-    private void Start()
+
+    #region Initialisation
+    protected sealed override void Start()
     {
-        _boxCollider = GetComponent<BoxCollider>(); 
-        _meshRenderer = GetComponent<MeshRenderer>();
-        _cooldownTimerRoutine = null;
-        _destroyRoutine = null;
-        _repairRoutine = null;  
+        base.Start(); 
+        Initialisation();
     }
 
-    #region Interfaces
-    // Responsible for taking damage and destroying object when Health < 0
-    public void ReceiveDamage(int damageReceived)
+    protected sealed override void Initialisation()
     {
-        Health -= damageReceived;
-        if (_repairRoutine != null)
-        {
-            StopCoroutine(_repairRoutine);
-            _repairParticleSystem.gameObject.SetActive(false);
-            _repairIcon.SetActive(false);
-            _repairRoutine = null;
-        }
-
-        if (Health < _destroyed)
-        {
-            ObjectDestroyed();
-        }
-        else
-        {
-            if(_cooldownTimerRoutine == null)
-            {
-                _cooldownTimerRoutine = StartCoroutine(CooldownTimerRoutine());
-            }
-            else
-            {
-                StopCoroutine(_cooldownTimerRoutine);
-                _cooldownTimerRoutine = StartCoroutine(CooldownTimerRoutine());
-            }
-        }
-    }
-    #endregion
-
-    #region Methods
-    // Responsible for the behaviour of an object being destroyed
-    private void ObjectDestroyed()
-    {
-        _destroyedParticleSystem.gameObject.SetActive(true);
-        _boxCollider.enabled = false;
-        _meshRenderer.enabled = false;
-        StopCoroutine(_cooldownTimerRoutine);
-        if (_destroyRoutine == null)
-            _destroyRoutine = StartCoroutine(PlayDestroyedParticleSystemRoutine());
-    }
-    #endregion
-
-    #region Coroutines
-    // Responsible for playing the destroyed particle system and disabling when finished
-    private IEnumerator PlayDestroyedParticleSystemRoutine()
-    {
-        while (_destroyedParticleSystem.isPlaying)
-        {
-            yield return null;
-        }
-        
-        _destroyedParticleSystem.gameObject.SetActive(false);
-        gameObject.SetActive(false);    
-        _destroyRoutine = null;
-    }
-
-    private IEnumerator CooldownTimerRoutine()
-    {
-        float timer = 0;
-
-        while (timer <= _idleRepairTimer)
-        {
-            timer += Time.deltaTime;
-            Debug.Log("Timer: " + timer);
-            yield return null;
-        }
-
-        if (_repairRoutine == null)
-            _repairRoutine = StartCoroutine(PlayRepairParticleSystemRoutine());
-
-        _cooldownTimerRoutine = null;
-    }
-
-    // Responsible for playing the repair particle system and disabling when object gets hit again or reach max health
-    private IEnumerator PlayRepairParticleSystemRoutine()
-    {
-        _repairParticleSystem.gameObject.SetActive(true);
-        _repairIcon.SetActive(true);
-
-        while(Health < MaxHealth)
-        {
-            float increment = RepairMultiplier * Time.deltaTime;
-            Health += increment;
-            Debug.Log("Health: " + Health);
-            yield return null;
-        }
-
-        _repairParticleSystem.gameObject.SetActive(false);
-        _repairIcon.SetActive(false);
-        Health = MaxHealth;
-        _repairRoutine = null;
+        MaxHealth = _barrierMaxHealth;
+        IdleRepairTimer = _barrierIdleRepairTimer;
+        RepairMultiplier = _barrierRepairMultiplier;
     }
     #endregion
 }
---
+++ Assets/Scripts/Environment/DamageableBase.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using UnityEngine;
+
+public abstract class DamageableBase : MonoBehaviour, IDamageable
+{
+    private int _maxHealth;             // Let the inheriting object determine it's Max Health
+    private const int _destroyed = 0;   // Const - Consistent across all types/instances at compile time
+
+    [SerializeField] private float _health;              // Variable for tracking game objects health
+    private float _idleRepairTimer;     // Let inherting object determine how much wait time there is before starting to repair
+    private float _repairMultiplier;    // Let inheriting object determine how fast/slow the object should repair
+
+    protected Coroutine _cooldownTimerRoutine;      // Internal bool timer to start repair on game object
+    protected Coroutine _destroyRoutine;            // Internal bool for destroying the game object
+    protected Coroutine _repairRoutine;             // Internal bool for repairing the game object
+
+    protected BoxCollider _boxCollider;
+    protected MeshRenderer _meshRenderer;
+
+    [SerializeField] protected ParticleSystem _repairParticleSystem;        // VFX - Healing 
+    [SerializeField] protected ParticleSystem _destroyedParticleSystem;     // VFX - Smoke
+    [SerializeField] protected GameObject _repairIcon;                      // Repair Icon that rotates above game object
+
+    #region Properties
+    protected int MaxHealth { get {  return _maxHealth; } set { _maxHealth = value; } }
+    protected int Destroyed { get { return _destroyed; } }
+    protected float Health { get { return _health; } set { _health = value; } } 
+    protected float IdleRepairTimer { get { return _idleRepairTimer; } set { _idleRepairTimer = value; } }  
+    protected float RepairMultiplier { get { return _repairMultiplier; } set { _repairMultiplier = value; } }   
+    #endregion
+
+
+
+    #region Initialisation
+    protected virtual void Start()
+    {
+        GrabComponents();
+        SetCoroutinesToNull();  
+    }
+
+    protected virtual void GrabComponents()                         // Grab the components from the game object
+    {
+        _boxCollider = GetComponent<BoxCollider>();
+        _meshRenderer = GetComponent<MeshRenderer>();   
+    }
+
+    protected virtual void SetCoroutinesToNull()                    // Set all Coroutine variables to null 
+    {
+        _cooldownTimerRoutine = null;
+        _destroyRoutine = null;
+        _repairRoutine = null;
+    }
+
+    protected abstract void Initialisation();                       // Let inheriting class set initialisation values
+    #endregion
+
+    #region Methods
+    protected virtual void DestroyThisObject()                      // Only called when an objects Health is below 0
+    {
+        _destroyedParticleSystem.gameObject.SetActive(true);        // Play destroyed smoke VFX
+        _boxCollider.enabled = false;                               // Turn off BoxCollider component - So we can't continue to shoot/hit it
+        _meshRenderer.enabled = false;                              // Turn off MeshRenderer component - So we can't see the object anymore
+        // TODO: Check if this matters
+        StopCoroutine(_cooldownTimerRoutine);                       // Stop running the Cooldown timer routine, otherwise it will play the other repair
+        if (_destroyRoutine == null)                                // Check if we aren't already destroying ourselves
+            _destroyRoutine = StartCoroutine(DestroyedObjectRoutine());
+    }
+    #endregion
+
+    #region Interfaces
+    // Responsible for taking damage and destroying object when Health < 0
+    public void ReceiveDamage(int damageReceived)
+    {
+        Health -= damageReceived;
+
+        // If the repair routine is already running, we need to stop it and wait for the timer again
+        if (_repairRoutine != null)
+        {
+            StopCoroutine(_repairRoutine);
+            _repairParticleSystem.gameObject.SetActive(false);
+            _repairIcon.SetActive(false);
+            _repairRoutine = null;
+        }
+
+        // Check if Health is below 0
+        if (Health < _destroyed)
+        {
+            DestroyThisObject();
+        }
+        else
+        {
+            if (_cooldownTimerRoutine == null)
+            {
+                _cooldownTimerRoutine = StartCoroutine(CooldownTimerRoutine());     // Start IdleRepairTimer
+            }
+            else
+            {
+                StopCoroutine(_cooldownTimerRoutine);                               // Stop the IdleRepairCoroutine and start it again (below)
+                _cooldownTimerRoutine = StartCoroutine(CooldownTimerRoutine());     // Start IdleRepairTimer again
+            }
+        }
+    }
+    #endregion
+
+    #region Coroutines
+    // Responsible for managing the timer of the game object and when it should start self-repairing
+    private IEnumerator CooldownTimerRoutine()
+    {
+        float timer = 0;
+
+        while (timer <= IdleRepairTimer)        // Don't do anything until this timer expires
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        if (_repairRoutine == null)
+            _repairRoutine = StartCoroutine(RepairVFXRoutine());
+
+        _cooldownTimerRoutine = null;
+    }
+
+    // Responsible for playing the destroyed particle system and disabling when finished
+    private IEnumerator DestroyedObjectRoutine()
+    {
+        while (_destroyedParticleSystem.isPlaying)                  // While the VFX is playing, don't do anything
+        {
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    // Responsible for playing the repair particle system and disabling when object gets hit again or reach max health
+    private IEnumerator RepairVFXRoutine()
+    {
+        _repairParticleSystem.gameObject.SetActive(true);           // Turn on VFX
+        _repairIcon.SetActive(true);                                // Turn on Repair Icon
+
+        while (Health < MaxHealth)                                  // While the game objects health is < MaxHealth, keep incrementing health and playing VFX
+        {
+            float increment = RepairMultiplier * Time.deltaTime;
+            Health += increment;
+            yield return null;
+        }
+
+        _repairParticleSystem.gameObject.SetActive(false);      // Turn off VFX
+        _repairIcon.SetActive(false);                           // Turn off Repair Icon
+        Health = MaxHealth;
+        _repairRoutine = null;
+    }
+    #endregion
+}
